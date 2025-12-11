da01d3a7da001acf1fc196dce35de5b33b9eb564
Anshul Negi <56960100+Anshul1310@users.noreply.github.com>
2025-12-11 11:23:43 +0530

    hi

--- index.js
+++ index.js
@@ -22,10 +22,9 @@ mongoose.connect(MONGO_URI || 'mongodb://localhost:27017/comedy-club')
 // --- ROUTES ---
 
 const initialCandidates = [
-    { name: "A", roll: "101", votes: 0 },
-    { name: "B", roll: "102", votes: 0 },
-    { name: "C", roll: "103", votes: 0 },
-    { name: "D", roll: "104", votes: 0 },
+    { name: "Anshul Negi", roll: "101", votes: 0 },
+    { name: "Tanishq Jhamar", roll: "102", votes: 0 },
+    { name: "Vrishank", roll: "103", votes: 0 },
    
 ];
 
